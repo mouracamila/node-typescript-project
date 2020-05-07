4ee5eec2cef0a0b1dcf256cb82838fbbfd783cab
Camila Moura <camilapaleo@gmail.com>
2020-05-07 17:56:42 +0200

    relating models

--- src/database/migrations/1588859626948-CreateUsers.ts
+++ src/database/migrations/1588859626948-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1588859626948 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
